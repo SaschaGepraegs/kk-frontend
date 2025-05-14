11c1a52d8cb34724447cca05ba6a791c37aef7a8
nillenia <alpayuerkuendag@gmail.com>
2025-05-14 10:53:24 +0200

    Umdreh Effekt hinzugefügt Beta 0.2.

--- Minigames/Memory+/pt_memory.js
+++ Minigames/Memory+/pt_memory.js
@@ -1,13 +1,18 @@
 const cards = document.getElementsByClassName("cards");
 
-// Loop through each card and attach the click event
+// Loop durch jede Karte
+// und füge einen Klick-Event-Listener hinzu
 for (let i = 0; i < cards.length; i++) {
     cards[i].onclick = function () {
-        // Check if the image source includes "Bc.jpg"
+        // Hingefügte Klasse "clicked" für Animation
+        // und um die Karte zu markieren
+        this.classList.toggle("clicked");
+
+        // Checken ob die Karte bereits aufgedeckt ist
         if (this.src.includes("Prototyp_Images/Bc.jpg")) {
-            this.src = "Prototyp_Images/Vc.png"; // Change to the other image
+            this.src = "Prototyp_Images/Vc.png"; // Wechsel zu Vc.png (zweite Karte)
         } else {
-            this.src = "Prototyp_Images/Bc.jpg"; // Revert back to the original image
+            this.src = "Prototyp_Images/Bc.jpg"; // Wechsel zu Bc.jpg (erste Karte)
         }
     };
 }
