9f815e9c463d91ebcae90610fdf3b8e9c28bdf3c
5Head-Felix <felix.schmidt@bvsg-nu.info>
2025-05-19 13:42:19 +0200

    Memory+ Punktezähler und Timer

--- Minigames/Memory+/pt_memory.js
+++ Minigames/Memory+/pt_memory.js
@@ -1,6 +1,10 @@
 const cards = document.getElementsByClassName("cards");
 let aktuellAufgedeckteKarten = []; // Array für aktuell aufgedeckte Karten; array.length ist Zähler für aufgedeckte Karten
-window.onload = function(){
+let punkte = 0; // Punktezähler
+let timer = 10; // Timer für die Spielzeit
+
+window.onload = function(){ // Funktion wird beim Laden der Seite aufgerufen
+    Timer(); // Timer starten
     const KartenInfos = [ // Array mit Karteninformationen
         {wert: 1, bild: "Prototyp_Images/Karte_König.jpg"},
         {wert: 1, bild: "Prototyp_Images/Karte_König.jpg"},
@@ -11,27 +15,14 @@ window.onload = function(){
     ];
     shuffleArray(KartenInfos); // Karten Mischen
     for (let i = 0; i < cards.length; i++) {
-        cards[i].KartenWert = KartenInfos[i].wert; // Kartenwert zuweisen
+        cards[i].KartenWert = KartenInfos[i].wert; // Wert der Karte zuweisen
         cards[i].KartenBild = KartenInfos[i].bild; // Bild der Karte zuweisen
         cards[i].src = "Prototyp_Images/Karte_Rückseite.jpg"; // Karte wird umgedreht (Rückseite)
         cards[i].aufgedeckt = false; // Karte ist nicht aufgedeckt
-        
-        cards[i].onclick = function () { // Loop durch jede Karte und füge einen Klick-Event-Listener hinzu
-            if (this.aufgedeckt == false && aktuellAufgedeckteKarten.length <2) { // Wenn die angeklickte Karte nicht aufgedeckt ist und weniger als 2 Karten aufgedeckt sind
-                KarteAufdecken.call(this); // Karte aufdecken
-                this.classList.toggle("clicked"); //Animation für Umdrehen
-                aktuellAufgedeckteKarten.push(this); // Karte in das Array der aktuell aufgedeckten Karten hinzufügen
-                if(aktuellAufgedeckteKarten.length == 2){ // Wenn 2 Karten aufgedeckt sind
-                    kartenVergleichen(); // Karten vergleichen
-                }
-            }else{
-                return; // Wenn die Karte bereits aufgedeckt ist, nichts tun
-            }
-    };
+        KartenEntsperren(); // Karten werden zu Spielbeginn entsperrt
     }
 }
 
-//Funktionen "KarteAufdecken" und "Karte Zudecken" sind ausgelagert, um späteren Spielmechanismus zu vereinfachen
 // Funktion, um die Karte aufzudecken
 function KarteAufdecken(){
     this.src = this.KartenBild; // Bild der Karte anzeigen
@@ -48,16 +39,43 @@ function KarteZudecken(){
 function kartenVergleichen(){
     setTimeout(function(){ // Timeout für 1 Sekunde
         if(aktuellAufgedeckteKarten[0].KartenWert == aktuellAufgedeckteKarten[1].KartenWert){ // Wenn die Karten gleich sind
-                    aktuellAufgedeckteKarten[0].onclick = null; // Klick-Event-Listener entfernen
-                    aktuellAufgedeckteKarten[1].onclick = null; // Klick-Event-Listener entfernen
+            aktuellAufgedeckteKarten[0].onclick = null; // Klick-Event-Listener entfernen
+            aktuellAufgedeckteKarten[1].onclick = null; // Klick-Event-Listener entfernen
+            punkte++; // Punkte erhöhen
+            document.getElementById("punkte").innerHTML = "Punkte: " + punkte; // Punkte anzeigen
         }else{
-                    KarteZudecken.call(aktuellAufgedeckteKarten[0]); // Karte zudecken
-                    KarteZudecken.call(aktuellAufgedeckteKarten[1]); // Karte zudecken
-                    aktuellAufgedeckteKarten[0].classList.toggle("clicked"); // Animation für Umdrehen
-                    aktuellAufgedeckteKarten[1].classList.toggle("clicked"); // Animation für Umdrehen
+            KarteZudecken.call(aktuellAufgedeckteKarten[0]); // Karte zudecken
+            KarteZudecken.call(aktuellAufgedeckteKarten[1]); // Karte zudecken
+            aktuellAufgedeckteKarten[0].classList.toggle("clicked"); // Animation für Umdrehen
+            aktuellAufgedeckteKarten[1].classList.toggle("clicked"); // Animation für Umdrehen
         }
         aktuellAufgedeckteKarten = []; // Array der aktuell aufgedeckten Karten zurücksetzen
     }, 1000); // Timeout von 1 Sekunde
+}
+
+// Funktion um die Karten zu sperren
+function KartenSperren(){
+    for (let i = 0; i < cards.length; i++) { // Loop durch alle Karten
+        cards[i].onclick = null; // Klick-Event-Listener entfernen
+    }
+}
+
+// Funktion um die Karten zu entsperren
+function KartenEntsperren(){
+    for(let i =0; i<cards.length; i++){ // Loop durch alle Karten
+        cards[i].onclick = function () { // Klick-Event-Listener hinzufügen
+            if (this.aufgedeckt == false && aktuellAufgedeckteKarten.length <2) { // Wenn die angeklickte Karte nicht aufgedeckt ist und weniger als 2 Karten aufgedeckt sind
+                KarteAufdecken.call(this); // Karte aufdecken
+                this.classList.toggle("clicked"); //Animation für Umdrehen
+                aktuellAufgedeckteKarten.push(this); // Karte in das Array der aktuell aufgedeckten Karten hinzufügen
+                if(aktuellAufgedeckteKarten.length == 2){ // Wenn 2 Karten aufgedeckt sind
+                    kartenVergleichen(); // Karten vergleichen
+                }
+            }else{
+                return; // Wenn die Karte bereits aufgedeckt ist, nichts tun
+            }
+        };
+    }
 }
 
 // Funktion um ein Array zu mischen
@@ -68,6 +86,20 @@ function shuffleArray(array) {
     array[i] = array[j];
     array[j] = temp;
   }
+}
+
+// Funktion für den Timer
+function Timer(){
+    setInterval(function(){ // Intervall für den Timer
+        if(timer > 0){ // Wenn der Timer größer als 0 ist
+            timer--; // Timer um 1 Sekunde verringern
+            document.getElementById("timer").innerHTML = "00:0" + timer; // Zeit anzeigen
+        }else{
+            clearInterval(this); // Intervall stoppen
+            KartenSperren(); // Karten sperren
+            document.getElementById("timer").innerHTML = "Zeit abgelaufen"; // Zeit abgelaufen anzeigen
+        }
+    }, 1000); // Intervall von 1 Sekunde
 }
 
 // Funktion um die Namen der Spieler zu speichern
