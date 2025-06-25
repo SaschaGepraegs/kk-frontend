e4cbcdb66a53b25a3d09b4d57d8c90029a30d790
5Head-Felix <felix.schmidt@bvsg-nu.info>
2025-06-25 12:27:00 +0200

    Mini-Update Asteroids-Seite

--- Minigames/Asteroids/asteroids.js
+++ Minigames/Asteroids/asteroids.js
@@ -3,10 +3,20 @@ window.onload = reset();
 function reset() {
     punkte = 10;
 }
+const hier = document.getElementById("hier");
+const momentle = document.getElementById("momentle");
+
+hier.addEventListener("click", () => {
+    spielBeenden();
+    hier.disabled = true;
+    momentle.style.display="flex";
+});
+
 async function spielBeenden() {
     // Lobby und Spielername aus localStorage herholen
     const lobby = localStorage.getItem("uic_gamepin") || "1111";
     const username = localStorage.getItem("uic_username") || "Name";
+
     try {
         await fetch(`https://kk-backend.vercel.app/addPointsToPlayer?lobby=${lobby}&spieler=${username}&punkte=${punkte}`);
         setTimeout(() => {window.location.assign("/System/pause.html");}, 3000);
