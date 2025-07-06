a260a5f5ba2980ce268bbc12da86183f79e575d9
dome2006 <dominykas.zak06@gmail.com>
2025-07-06 14:34:55 +0200

    Erster Versuch von Gartic Phone

---
+++ Minigames/Gartic Phone/GP.js
@@ -0,0 +1,31 @@
+const canvas = document.getElementById("canvas");
+const Werkzeuge = document.getElementById("Werkzeuge");
+const ctx = canvas.getContext("2d");
+
+const canvasOffsetX = canvas.offsetLeft;
+const canvasOffsetY = canvas.offsetTop;
+
+canvas.width = window.innerWidth - canvasOffsetX;
+canvas.height = window.innerHeight - canvasOffsetY;
+
+
+let isPainting = false;
+let lastlineWidth = 5;
+let startX = 0;
+let startY = 0;
+
+Werkzeuge.addEventListener("click", (e) => {
+    if (e.target.id === "Loeschen") {
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+    } else if (e.target.id === "Zuruecksetzen") {
+        ctx.restore();
+    }
+});
+
+Werkzeuge.addEventListener("change", (e) => {
+    if (e.target.id === "Stift") {
+        ctx.strokeStyle = e.target.value;
+    } else if (e.target.id === "Dicke") {
+        lineWidth = e.target.value;
+    }
+});
